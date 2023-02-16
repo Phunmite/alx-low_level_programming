208d0406a46f17e75a2cda4090c3f8ca530ba89e
phunmite <funscofumex@gmail.com>
2023-02-16 14:10:15 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - this is the main function
+ * Return: 0 when successful
+*/
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}	
