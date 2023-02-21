d7d3501aae7add7caf33dc999171a728a41595bd
phunmite <funscofumex@gmail.com>
2023-02-21 13:16:18 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
+
 /**
- * main - main function 
- * _putchar: print the alphabets putchar
- *
+ * main - Print _putchar
  * Return: Always 0
  */
-
 int main(void)
 {
 	_putchar('_');
